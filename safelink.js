c62e5c88a4313044a25977258a63f46da7d5d883
mushabhidayat <52373296+mushabhidayat@users.noreply.github.com>
2020-11-22 02:35:57 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="what-are-hipaa-laws.html"}
 if(x=="2"){xxx="value-your-health-buy-insurance.html"}
 if(x=="3"){xxx="for-in-between-times-short-term-health.html"}
 if(x=="4"){xxx="stay-legal-avoiding-insurance-fraud.html"}
-output.value = "https://mushabsafelink.blogspot.com/2020/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="noopener";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+output.value = "https://safelink.mushab.or.id//2020/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="noopener";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
