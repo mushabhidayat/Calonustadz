38496f5e6144398b0f452135de0a71e2d267e71d
mushabhidayat <52373296+mushabhidayat@users.noreply.github.com>
2024-02-21 20:16:57 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="what-are-hipaa-laws.html"}
+if(x=="1"){xxx="how-to-solve-eating-difficulty-in.html"}
 if(x=="2"){xxx="value-your-health-buy-insurance.html"}
 if(x=="3"){xxx="for-in-between-times-short-term-health.html"}
-if(x=="4"){xxx="stay-legal-avoiding-insurance-fraud.html"}a_to_ve[i].href="https://safelink.mushab.or.id/2020/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="noopener";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="stay-legal-avoiding-insurance-fraud.html"}a_to_ve[i].href="https://id.mushab.or.id/2024/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="noopener";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
